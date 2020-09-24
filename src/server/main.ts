0b4e8f806cc365720278675730ea4ba2ad8e8252
dnacu <winteri1276@gmail.com>
2020-09-24 18:02:49 +0900

    docs

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,9 +1,13 @@
 import { PORT, app } from './app'
 import SocketIO from 'socket.io'
+import http from 'http'
 
-const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+const a = http.createServer(app)
+const io = SocketIO(a)
+a.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+// const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
 
-const io = SocketIO(server, { transports: ['websocket'] })
+// const io = SocketIO(server)
 
 io.on('connect', (socket) => {
   socket.on('card', (...data) => {
